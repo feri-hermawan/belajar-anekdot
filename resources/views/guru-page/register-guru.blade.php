eadc1dfa12a699a4d90d04f789cc2783052b844d
natsuki-yasumi <ferihermawan007@gmail.com>
2024-05-25 19:17:23 +0700

    mengganti foto di halaman registrasi guru

--- resources/views/guru-page/register-guru.blade.php
+++ resources/views/guru-page/register-guru.blade.php
@@ -17,7 +17,7 @@
         <div class="container-fluid h-custom mb-4">
             <div class="row d-flex justify-content-center align-items-center h-100">
                 <div class="col-md-9 col-lg-6 col-xl-5">
-                    <img src="/img/register-illustration.png"
+                    <img src="/img/teacher-illustration.png"
                          class="img-fluid" alt="Sample image">
                 </div>
                 <div class="col-md-8 col-lg-6 col-xl-4 offset-xl-1">
